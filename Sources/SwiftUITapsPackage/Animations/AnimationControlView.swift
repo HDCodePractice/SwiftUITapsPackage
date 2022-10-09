3a5276baa21d37ea8ace4ad4bb7ee45d72df9aa6
老房东 <huangdong@gmail.com>
2022-10-09 08:56:49 -0400

    Add animation Tap

---
+++ Sources/SwiftUITapsPackage/Animations/AnimationControlView.swift
@@ -0,0 +1,126 @@
+//
+//  SwiftUIView.swift
+//
+//
+//  Created by 老房东 on 2022-10-09.
+//
+
+import SwiftUI
+
+struct AnimationControlView: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                HeadlineView(
+                    title: "animation",
+                    url: "https://developer.apple.com/documentation/swiftui/view/animation(_:value:)",
+                    description: String(localized: "Applies the given animation to this view when the specified value changes.")
+                )
+                SimpleAnimationSampleView()
+                Divider()
+            }
+            .padding()
+        }
+    }
+}
+
+private struct SimpleAnimationSampleView: View {
+    var code: String {
+        """
+        struct SimpleAnimationSampleView: View {
+            @State var isAnimation = false
+            var body: some View {
+                VStack{
+                    ZStack{
+                        Text("easeIn").font(.footnote)
+                        Circle()
+                            .fill(.cyan)
+                            .frame(width: 30)
+                            .animation(.easeIn(duration: \(duration.valueString), value: isAnimation)
+                            .offset(x: isAnimation ? 150 : -150)
+                    }
+                    ZStack{
+                        Text("easeOut").font(.footnote)
+                        Circle()
+                            .fill(.cyan)
+                            .frame(width: 30)
+                            .animation(.easeOut(duration: \(duration.valueString)), value: isAnimation)
+                            .offset(x: isAnimation ? 150 : -150)
+                    }
+                    ZStack{
+                        Text("easeInOut").font(.footnote)
+                        Circle()
+                            .fill(.cyan)
+                            .frame(width: 30)
+                            .animation(.easeInOut(duration: \(duration.valueString)), value: isAnimation)
+                            .offset(x: isAnimation ? 150 : -150)
+                    }
+                    ZStack{
+                        Text("linear").font(.footnote)
+                        Circle()
+                            .fill(.cyan)
+                            .frame(width: 30)
+                            .animation(.linear(duration: \(duration.valueString)), value: isAnimation)
+                            .offset(x: isAnimation ? 150 : -150)
+                    }
+                    Button("Start Animation"){
+                        isAnimation.toggle()
+                    }
+                }
+            }
+        }
+        """
+    }
+
+    @State var duration = DoubleOption(name: "duration", value: 0.5, range: 0 ... 2)
+    @State var isAnimation = false
+    var body: some View {
+        VStack {
+            Text("Call animation function")
+                .font(.title2)
+            CodePreviewView(code: code)
+            ZStack {
+                Text("easeIn").font(.footnote)
+                Circle()
+                    .fill(.cyan)
+                    .frame(width: 30)
+                    .animation(.easeIn(duration: duration.value), value: isAnimation)
+                    .offset(x: isAnimation ? 150 : -150)
+            }
+            ZStack {
+                Text("easeOut").font(.footnote)
+                Circle()
+                    .fill(.cyan)
+                    .frame(width: 30)
+                    .animation(.easeOut(duration: duration.value), value: isAnimation)
+                    .offset(x: isAnimation ? 150 : -150)
+            }
+            ZStack {
+                Text("easeInOut").font(.footnote)
+                Circle()
+                    .fill(.cyan)
+                    .frame(width: 30)
+                    .animation(.easeInOut(duration: duration.value), value: isAnimation)
+                    .offset(x: isAnimation ? 150 : -150)
+            }
+            ZStack {
+                Text("linear").font(.footnote)
+                Circle()
+                    .fill(.cyan)
+                    .frame(width: 30)
+                    .animation(.linear(duration: duration.value), value: isAnimation)
+                    .offset(x: isAnimation ? 150 : -150)
+            }
+            DoubleOptionView(option: $duration)
+            Button("Start Animation") {
+                isAnimation.toggle()
+            }
+        }
+    }
+}
+
+struct AnimationControlView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationControlView()
+    }
+}
